c2fd3f460f7e5199f61b25b68ab8a4b3187f4e0d
jounaidbeaufils <jounaidbeaufils@gmail.com>
2023-04-18 19:05:41 +0200

    added untested & unused variance estimator

--- angorapy/agent/variance.py
+++ angorapy/agent/variance.py
@@ -1,4 +1,6 @@
 import numpy as np
+from angorapy.common.const import NP_FLOAT_PREC
+from scipy.signal import lfilter
 
 """Variance methods providing functionality to the agent."""
 
@@ -20,3 +22,26 @@ def future_reward_variance(rewards):
     n_list = [x for x in range(n, -1, -1)]
 
     return list(zip(variance,n_list))
+
+# untested function, currently unused. 
+def estimate_episode_variance(rewards, values, discount, lam):
+    """estimate episode variance in the style of estimate_episode_advantages()"""
+    #gets pooled variance. equivalent to deltas
+    arrays = np.array(rewards, values[1:], values[:-1])
+
+    # Calculate n_total for each column
+    n_total = np.sum(arrays[:, :, 1], axis=0)
+
+    # Calculate variances for each column
+    variances = arrays[:, :, 0] * (arrays[:, :, 1] - 1)
+
+    # Sum variances for each column
+    sum_variances = np.sum(variances, axis=0)
+
+    # Calculate pooled variances
+    pooled_var = sum_variances / (n_total - len(arrays))
+
+    # Combine pooled_var and n_total into a single array
+    discounted_var = lfilter([1], [1, float(-(discount * lam))], pooled_var[::-1], axis=0)[::-1].astype(NP_FLOAT_PREC)
+    
+    return np.vstack((discounted_var, n_total))
