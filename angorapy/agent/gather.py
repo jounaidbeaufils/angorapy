Fixed None type gradient error

angorapy/agent/gather.py
@@ -258,7 +258,7 @@ class VarGatherer(Gatherer):
     # define __init__() overide to set default variance stragy
     def __init__(self, worker_id: int, exp_id: int, distribution: BasePolicyDistribution, horizon: int, discount: float, lam: float, subseq_length: int):
         super().__init__(worker_id, exp_id, distribution, horizon, discount, lam, subseq_length)
-        self.var_strategy = variance.absolute
+        self.var_strategy = variance.future_reward_variance
 
     # define assign_strategy() to set desired strategy
     def assign_var_strategy(self, strategy):
@@ -371,7 +371,7 @@ class VarGatherer(Gatherer):
                 else:
                     advantages.append(episode_advantages)
                     # (Jounaid)
-                    pseudo_variances.append(episode_variances)
+                    pseudo_variances.extend(episode_variances)
 
                 # reset environment to receive next episodes initial state
                 state = env.reset()
@@ -407,7 +407,7 @@ class VarGatherer(Gatherer):
             else:
                 advantages.append(leftover_advantages)
                 # (Jounaid)
-                pseudo_variances.append(leftover_pseudo_variance)
+                pseudo_variances.extend(leftover_pseudo_variance)
 
         # if not recurrent, fill the buffer with everything we gathered
         if not is_recurrent:
@@ -417,7 +417,7 @@ class VarGatherer(Gatherer):
             advantages = np.hstack(advantages).astype("float32")
             returns = advantages + values[:-1]
             # (Jounaid)
-            pseudo_variances = np.hstack(pseudo_variances).astype("float32")
+            pseudo_variances = np.array(pseudo_variances).astype("float32")
 
             buffer.fill(states,
                         np.array(actions, dtype="float32" if is_continuous else "int32"),

angorapy/agent/ppo/loss.py
@@ -100,7 +100,7 @@ def entropy_bonus(policy_output: tf.Tensor, distribution) -> tf.Tensor:
     return tf.reduce_mean(distribution.entropy(policy_output))
 
 @tf.function
-def policy_pseudo_var_loss(pseudo_variance: tf.Tensor,
+def pseudo_var_loss_abs(pseudo_variance: tf.Tensor,
                 mask: tf.Tensor,
                 is_recurrent: bool) -> tf.Tensor:
     """pseudo variance as a measurure of reward variability.
@@ -124,3 +124,58 @@ def policy_pseudo_var_loss(pseudo_variance: tf.Tensor,
         return tf.reduce_sum(clipped_masked) / tf.reduce_sum(tf.cast(mask, tf.float32))
     else:
         return tf.reduce_mean(pseudo_variance)
+    
+@tf.function
+def pseudo_var_loss(pseudo_var_predictions: tf.Tensor,
+               old_pseudo_var: tf.Tensor,
+               true_pseudo_var: tf.Tensor,
+               mask: tf.Tensor,
+               clip: bool,
+               clipping_bound: tf.Tensor,
+               is_recurrent: bool) -> tf.Tensor:
+    """Loss of the critic network as squared error between the prediction and the sampled future return. In the
+    recurrent case a mask is calculated based on 0 values in the old_action_prob tensor. This mask is then applied
+    in the mean operation of the loss.
+
+    Args:
+      pseudo_var_predictions (tf.Tensor): variance prediction by the current critic network
+      old_pseudo_var (tf.Tensor): old variance prediction by the old critic network during gathering
+      true_pseudo_var (tf.Tensor): calculated variance in episode
+      clip (object): (Default value = True) value loss can be clipped by same range as policy loss
+
+    Returns:
+      squared error between prediction and return
+    """
+    tf.debugging.assert_all_finite(pseudo_var_predictions, "pseudo_var_predictions is not all finite!")
+    # tf.debugging.assert_all_finite(old_pseudo_var, "old_pseudo_var is not all finite!")
+    tf.debugging.assert_all_finite(true_pseudo_var, "true_pseudo_var is not all finite!")
+
+    # Convert list of tuples to separate lists
+    a_list, b_list = tf.split(true_pseudo_var, num_or_size_splits=2, axis=1)
+
+    # Convert lists to tensors
+    a_tensor = tf.squeeze(a_list)
+    b_tensor = tf.squeeze(b_list)
+
+    # Subtract a_tensor from the first column of tensor and b_tensor from the second column of tensor
+    diff = tf.subtract(pseudo_var_predictions, tf.stack([a_tensor, b_tensor], axis=1))
+
+
+    error = tf.square(diff)
+
+    if clip:
+        raise NotImplementedError("cliping not added yet in recurrent network")  
+        # clips value error to reduce variance
+        clipped_values = old_pseudo_var + tf.clip_by_value(pseudo_var_predictions - old_pseudo_var, -clipping_bound, clipping_bound)
+        clipped_error = tf.square(clipped_values - true_pseudo_var)
+        error = tf.maximum(clipped_error, error)
+
+    if is_recurrent:
+        
+        raise NotImplementedError("pseudo_variance not available in recurrent network")
+
+        # apply mask over the old values
+        error_masked = tf.where(mask, error, 1)  # masking with tf.where because inf * 0 = nan...
+        return (tf.reduce_sum(error_masked) / tf.reduce_sum(tf.cast(mask, tf.float32))) * 0.5
+    else:
+        return tf.reduce_mean(error) * 0.5

angorapy/agent/ppo/optim.py
@@ -126,7 +126,7 @@ def learn_on_batch_with_var(batch,
         state_batch = {fname: f for fname, f in batch.items() if fname in Sensation.sense_names}
         old_values = batch["value"]
 
-        policy_output, _ ,value_output = joint(state_batch, training=True)
+        policy_output, pseudo_var_output ,value_output = joint(state_batch, training=True)
 
         if continuous_control:
             # if action space is continuous, calculate PDF at chosen action value
@@ -154,17 +154,19 @@ def learn_on_batch_with_var(batch,
                                      is_recurrent=is_recurrent)
         entropy = loss.entropy_bonus(policy_output=policy_output,
                                      distribution=distribution)
-        pseudo_variance_loss = loss.policy_pseudo_var_loss(pseudo_variance= batch["pseudo_variance"],
-                                                           mask=batch["mask"],
-                                                           is_recurrent=is_recurrent)
+        pseudo_variance_loss = loss.pseudo_var_loss(pseudo_var_predictions=pseudo_var_output,
+                                    old_pseudo_var= None, #clipping not yet implemented
+                                    true_pseudo_var=batch["pseudo_variance"],
+                                    mask= batch["mask"],
+                                    clip = False, #clipping not yet implemented
+                                    clipping_bound= clipping_bound,
+                                    is_recurrent=is_recurrent)
 
         # combine weighted losses
         total_loss = policy_loss + tf.multiply(c_value, value_loss) - tf.multiply(c_entropy, entropy) + tf.multiply(0.001, pseudo_variance_loss)
 
     # calculate the gradient of the joint model based on total loss
     gradients = tape.gradient(total_loss, joint.trainable_variables)
-
-    print(gradients)
 
     for i, gradient in enumerate(gradients):
         tf.debugging.assert_all_finite(gradient, f"Gradient {i} is nan/inf")

angorapy/models/simple.py
@@ -111,10 +111,10 @@ def build_var_ffn_models(env: BaseWrapper,
     if not shared:
         pseudo_variance_latent = _build_encoding_sub_model(inputs.shape[1:], None, layer_sizes=layer_sizes,
                                                  name="pseudo_variance_encoder")(inputs)
-        pseudo_variance_out = tf.keras.layers.Dense(3, kernel_initializer=tf.keras.initializers.Orthogonal(1.0),
+        pseudo_variance_out = tf.keras.layers.Dense(2, kernel_initializer=tf.keras.initializers.Orthogonal(1.0),
                                           bias_initializer=tf.keras.initializers.Constant(0.0))(pseudo_variance_latent)
     else:
-        pseudo_variance_out = tf.keras.layers.Dense(3, input_dim=layer_sizes[-1],
+        pseudo_variance_out = tf.keras.layers.Dense(2, input_dim=layer_sizes[-1],
                                           kernel_initializer=tf.keras.initializers.Orthogonal(1.0),
                                           bias_initializer=tf.keras.initializers.Constant(0.0))(latent)
 