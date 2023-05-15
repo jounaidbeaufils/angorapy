188dfebe65c31983af57f27c592d6983da7335c8
weidler <web@tonioweidler.de>
2023-05-15 08:46:58 +0200

    Increased worker in HP optim script to 12.

--- Experiments/fine_tune.py
+++ Experiments/fine_tune.py
@@ -1,10 +1,10 @@
 import os
-import time
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 try:
     from mpi4py import MPI
+
     is_root = MPI.COMM_WORLD.rank == 0
 except:
     is_root = True
@@ -13,35 +13,34 @@ from angorapy.common.policies import BetaPolicyDistribution
 from angorapy.common.transformers import RewardNormalizationTransformer, StateNormalizationTransformer
 from angorapy.common.wrappers import make_env
 
-from angorapy.agent.ppo_agent import PPOAgent,VarPPOAgent
-from angorapy.agent.gather import VarGatherer, VarGathererAbs, VarGathererNoPreds
+from angorapy.agent.ppo_agent import VarPPOAgent
+from angorapy.agent.gather import VarGatherer
 from angorapy.models import build_var_ffn_models
 
 import statistics
-import panda_gym
 import numpy as np
 import itertools
 
 import argparse
-import ast
 
 LOG_FILE_PATH = "experiments_log.txt"
 
 parser = argparse.ArgumentParser()
 
 ## required arguements ##
-parser.add_argument("perm_list",type=int, nargs='+')
+parser.add_argument("perm_list", type=int, nargs='+')
 args = parser.parse_args()
 
 
 def permutation():
-    #generate permutations of hyperparameters
+    # generate permutations of hyperparameters
     c_entropy = [0.1, 0]
     c_var = np.linspace(1.03, 0.9, num=5)
     v_discount = np.linspace(0.1, 0.02, num=5)
 
     # return the Cartesian product (every combination) of the three sets
     return list(itertools.product(c_entropy, c_var, v_discount))
+
 
 def train_agent(i: int, permutations):
     # transformers
@@ -52,20 +51,21 @@ def train_agent(i: int, permutations):
     # policy distribution
     distribution = BetaPolicyDistribution(env)
 
-    agent = VarPPOAgent(model_builder=build_var_ffn_models, environment=env, 
-                            horizon=512,
-                            workers=1,
-                            c_entropy=permutations[i][0],
-                            c_var=permutations[i][1],
-                            var_discount=permutations[i][2], 
-                            distribution=distribution,
-                            var_by_adv=True,
-                            var_pred=True)
+    agent = VarPPOAgent(model_builder=build_var_ffn_models, environment=env,
+                        horizon=512,
+                        workers=12,
+                        c_entropy=permutations[i][0],
+                        c_var=permutations[i][1],
+                        var_discount=permutations[i][2],
+                        distribution=distribution,
+                        var_by_adv=True,
+                        var_pred=True)
     agent.assign_gatherer(VarGatherer)
 
     # Run a training loop
     agent.drill(n=300, epochs=3, batch_size=64, save_every=5)
     return agent
+
 
 def evaluate_agent(agent):
     #  evaluate the performance of the agent
