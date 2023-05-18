3b71ac9cf6a3d20e6fed9fe6d3c610417e810a39
weidler <web@tonioweidler.de>
2023-05-18 13:39:53 +0200

    removed run_experiment file writing, does not work with server.

--- Experiments/execute_on_cscs.sh
+++ Experiments/execute_on_cscs.sh
@@ -3,3 +3,29 @@ for i in {0..49}
 do
   bash jsubmit.sh -p "Experiments/fine_tune.py $i" -t 24:00:00
 done
+
+# run 10 iterations of each model type
+for i in {0..9}
+do
+  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models var_pred --save_interval 0 --n 500" -t 05:00:00
+done
+
+for i in {0..9}
+do
+  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models var_no_pred --save_interval 0 --n 500" -t 05:00:00
+done
+
+for i in {0..9}
+do
+  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models abs --save_interval 0 --n 500" -t 05:00:00
+done
+
+for i in {0..9}
+do
+  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models noise --save_interval 0 --n 500" -t 05:00:00
+done
+
+for i in {0..9}
+do
+  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models ori --save_interval 0 --n 500" -t 05:00:00
+done
