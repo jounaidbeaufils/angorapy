4e3d1bd37c4ac909d3c4cd26635f0b291f7393f3
weidler <web@tonioweidler.de>
2023-05-25 15:13:01 +0200

    Scripts

--- Experiments/execute_on_cscs.sh
+++ Experiments/execute_on_cscs.sh
@@ -7,25 +7,20 @@ done
 # run 10 iterations of each model type
 for i in {0..9}
 do
-  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models var_pred --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_pred --div true --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_pred --div true --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_pred --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_pred --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_no_pred --div true --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_no_pred --div true --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_no_pred --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid var_no_pred --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid abs --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid abs --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid noise --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid noise --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid ori --env PandaPushDense-v2 --save_interval 0 --n 500" -t 05:00:00
+  bash jsubmit.sh -p "Experiments/run_experiment.py jounaid ori --env PandaReachDense-v2 --save_interval 0 --n 500" -t 05:00:00
 done
 
-for i in {0..9}
-do
-  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models var_no_pred --save_interval 0 --n 500" -t 05:00:00
-done
 
-for i in {0..9}
-do
-  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models abs --save_interval 0 --n 500" -t 05:00:00
-done
-
-for i in {0..9}
-do
-  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models noise --save_interval 0 --n 500" -t 05:00:00
-done
-
-for i in {0..9}
-do
-  bash jsubmit.sh -p "Experiments/run_experiment.py var_ppo_models ori --save_interval 0 --n 500" -t 05:00:00
-done
